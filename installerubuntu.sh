5e6e0d28c172d01d4f73ef0178c2f108c7cd42db
belajarit45 <144545476+belajarit45@users.noreply.github.com>
2024-07-09 16:14:51 +0700

    Create installerubuntu.sh

---
+++ installerubuntu.sh
@@ -0,0 +1,10 @@
+pkg update && pkg upgrade -y
+pkg install wget -y
+pkg install proot -y
+pkg install git -y
+cd ~
+git clone https://github.com/MFDGaming/ubuntu-in-termux.git
+cd ubuntu-in-termux
+chmod +x ubuntu.sh
+./ubuntu.sh -y
+./startubuntu.sh

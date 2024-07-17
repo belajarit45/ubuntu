178b09f1be8bc8d29afb35b6f30385ce4f8caa58
belajarit45 <144545476+belajarit45@users.noreply.github.com>
2024-07-17 07:56:00 +0700

    Update installdockerpandora.sh

--- installdockerpandora.sh
+++ installdockerpandora.sh
@@ -1,6 +1,6 @@
 1 sudo apt update 
 2 sudo apt install apt-transport-https ca-certificates curl software-properties-common 
-3 curl -fsSL https://download.docker.com/linux/ubu... | sudo apt-key add - 
+3 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
 4 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" 
 5 sudo apt update 
 6 apt-cache policy docker-ce 
